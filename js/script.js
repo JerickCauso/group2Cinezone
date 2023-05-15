d641438f624c32c92399e51217822bed725a9e24
JerickCauso <jergcauso@gmail.com>
2023-05-15 10:44:23 +0800

    jerick

--- js/script.js
+++ js/script.js
@@ -178,6 +178,7 @@ function movieDetails() {
       const nowPlaying = document
         .querySelector("#movie-details")
         .appendChild(div);
+      displayBackdrop("movie", responsedata.backdrop_path);
 
       div.innerHTML = `
       <div class="details-top">
@@ -230,6 +231,7 @@ function tvShowDetails() {
       const nowPlaying = document
         .querySelector("#show-details")
         .appendChild(div);
+      displayBackdrop("tv", responsedata.backdrop_path);
 
       div.innerHTML = `
       <div class="details-top">
@@ -257,12 +259,33 @@ function tvShowDetails() {
     });
 }
 
+// Display Backdrop
+function displayBackdrop(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
+}
+
 function init() {
   switch (pageSet.currentPage) {
     case "/":
     case "/index.html":
       displaySlider();
-      // nowPlaying();
       moviesPopular();
       break;
     case "/shows.html":
